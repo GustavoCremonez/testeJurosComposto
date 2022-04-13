adddfe8f4ebdbb12ad6d8d8ee74c70e9136f43de
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-04-12 21:49:19 -0300

    basic error handling and advancing in the api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const formFee = document.querySelector('.formFees')
+const firstPage = document.querySelector('.firstPage')
+const secondPage = document.querySelector('.secondPage')
+const divSecondPage = document.querySelector('.contentSecondPage')
 const simulate = document.querySelector('.simulate')
 const simulateAgain = document.querySelector('.simulateAgain')
-const urlAPI = api.mathjs.org/v4/
+const urlAPI = "http://api.mathjs.org/v4/"
 
 
 
@@ -18,7 +21,43 @@ formFee.addEventListener('submit', function(e){
 })
 
 const apiFetch = (name, monthlyPayment, time, fees) => {
-    const expr = { "expr" : `${monthlyPayment} * (((1 + ${fees}) ^ [${time * 12} - 1) / ${fees}) `}
+    const expr = { 
+        "expr" : [`${monthlyPayment} * (((1 + ${fees}) ^ [${time * 12} - 1) / ${fees})`],
+        "precision": 3
+    }
 
-    console.log(JSON.stringify(expr))
+    const init = {
+        method: 'POST',
+        headers: {
+            "content-type": 'application/json'
+        },
+        body: encodeURI(expr)
+    }
+    fetch(urlAPI, init)
+        .then(toJson)
+        .then(makeHtmlElement)
+        .catch(apiErr)
+}
+
+const toJson = (response) =>{
+    return response.json()
+}
+
+const makeHtmlElement = function(data){
+    console.log(data)
+}
+
+const apiErr = (response) => {
+    firstPage.classList.add('hidden')
+    secondPage.classList.remove('hidden')
+    divSecondPage.innerHTML = `
+        <h3> Erro </h3>
+        <span> Algo deu errado, tente novamente mais tarde!</span>
+
+        <h5>Error = ${response}</h5>
+    `
+
+    simulateAgain.addEventListener('click', function(e){
+        location.reload()
+    })
 }
